a25a564e32033c7fece9d3e90297efaafdb70f37
Kos Perun <kosperun@gmail.com>
2024-08-21 18:25:10 -0400

    Add neo-tree configs to show/hide dotfiles

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,18 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        always_show = { -- remains visible even if other settings would normally hide it
+          ".env",
+        },
+        hide_by_name = {
+          ".vscode",
+          ".git",
+        },
+      },
+    },
     window = {
       mappings = {
         ["Y"] = function(state)
