6ecaec09ce1b4ebf9318add3a25972865fe3d5db
Kostiantyn Perun <kosperun@gmail.com>
2024-11-23 20:15:16 -0500

    Add auto-save autocmd

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,14 +1,36 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Auto reload the buffer after file was changed elsewhere
 -- Trigger `checktime` on certain events to check if the file has changed
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
   command = "if mode() != 'c' | checktime | endif",
 })
-
 -- Notification after file change
 vim.api.nvim_create_autocmd("FileChangedShellPost", {
   callback = function()
     vim.cmd('echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None')
+  end,
+})
+
+-- Auto-save buffer to file
+local function clear_cmdarea()
+  vim.defer_fn(function()
+    vim.api.nvim_echo({}, false, {})
+  end, 800)
+end
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged", "BufLeave", "FocusLost" }, {
+  callback = function()
+    if #vim.api.nvim_buf_get_name(0) ~= 0 and vim.bo.buflisted then
+      vim.cmd("silent w")
+
+      local time = os.date("%I:%M %p")
+
+      -- print nice colored msg
+      vim.api.nvim_echo({ { "󰄳", "LazyProgressDone" }, { " file autosaved at " .. time } }, false, {})
+
+      clear_cmdarea()
+    end
   end,
 })
