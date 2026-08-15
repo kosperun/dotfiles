86124a41b3da7f59c94fc881370e011876fc488f
Kostiantyn Perun <kosperun@gmail.com>
2026-08-15 11:46:59 -0400

    Correct gU URL for work

--- nvim/lua/plugins/git-plugins/git-blame.lua
+++ nvim/lua/plugins/git-plugins/git-blame.lua
@@ -8,4 +8,16 @@ return {
   keys = {
     { "<leader>gt", "<cmd>GitBlameToggle<cr>", desc = "Git Blame Toggle CUSTOM" },
   },
+  config = function(_, opts)
+    require("gitblame").setup(opts)
+
+    -- Work repos use a "github.com-<alias>" SSH host alias to pick the
+    -- right key. That alias isn't a real domain, so strip it back to
+    -- github.com before opening a browser URL.
+    local utils = require("gitblame.utils")
+    local original_launch_url = utils.launch_url
+    utils.launch_url = function(url)
+      original_launch_url((url:gsub("github%.com%-%w+", "github.com")))
+    end
+  end,
 }
